43aa2eb00462b3987d85529b368d8dda826b0869
Kevin Chen <kyc2915@gmail.com>
2023-08-04 18:27:52 -0700

    test montgomery multiplication

--- src/multiplication_test.rs
+++ src/multiplication_test.rs
@@ -1,96 +1,82 @@
+const R: u64 = 1u64 << 32;
+
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+fn find_linear_combination(a: i64, b: i64) -> Point {
+    if b == 0 {
+        Point {
+            x: if a >= 0 { 1 } else { -1 },
+            y: 0,
+        }
+    } else {
+        let Point { x, y } = find_linear_combination(b, a % b);
+        Point {
+            x: y,
+            y: x - a / b * y,
+        }
+    }
+}
+
+fn mod_inverse(a: u64, n: u64) -> u64 {
+    let inverse = find_linear_combination(a as i64, n as i64).x;
+    if inverse < 0 {
+        (inverse + n as i64) as u64
+    } else {
+        inverse as u64
+    }
+}
+
+fn redc(t: u64, n: u64, np: u64) -> u64 {
+    let tt = (t + t % R * np % R * n) / R;
+    if tt < n {
+        tt
+    } else {
+        tt - n
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use bnum::types::I8192;
-    use crypto_bigint::{Random, Uint, Wrapping, U8192};
-    use ibig::ibig;
-    use rand::{rngs::StdRng, Rng, SeedableRng};
-    use rug::rand::RandState;
-    use rug::{Assign, Integer};
-    use std::time::{Duration, Instant};
+    use super::*;
+    use rand::{rngs::StdRng, SeedableRng};
+    use std::time::Instant;
+
+    use crate::multiplication_test::mod_inverse;
 
     /*
-    Time elapsed: 50.339727ms
-    test multiplication_test::tests::test_rug ... ok
-    Time elapsed: 107.228613ms
-    test multiplication_test::tests::test_ibig ... ok
-    Time elapsed: 166.851162ms
-    test multiplication_test::tests::test_crypto_bigint ... ok
-    Time elapsed: 103.439212ms
-    test multiplication_test::tests::test_bnum ... ok
+    This takes ~9.5s with normal multiplication, and ~8.1s with Montgomery multiplication, a 15% speedup.
      */
-    const COUNT: usize = 10000;
-    const NUM_WORDS: usize = 128;
+    const COUNT: usize = 1000000000;
+    const P: u64 = 1000000007;
 
     #[test]
-    fn test_ibig() {
+    fn test_montgomery() {
         let mut rng = StdRng::seed_from_u64(0);
 
-        let mut total_duration = Duration::ZERO;
-
-        let mut sum_products = ibig!(1);
+        let mut nums = vec![];
+        let mut num = 2;
         for _ in 0..COUNT {
-            let a = rng.gen_range(ibig!(1)..ibig!(2).pow(64 * NUM_WORDS));
-            let b = rng.gen_range(ibig!(1)..ibig!(2).pow(64 * NUM_WORDS));
-            let start = Instant::now();
-            sum_products += a * b;
-            total_duration += start.elapsed();
+            nums.push(num);
+            num = num * num % P + 1;
         }
 
-        println!("Time elapsed: {:?}", total_duration);
-    }
+        let start = Instant::now();
 
-    #[test]
-    fn test_bnum() {
-        let mut rng = StdRng::seed_from_u64(0);
-
-        let mut total_duration = Duration::ZERO;
-
-        let mut sum_products = I8192::ONE;
-        for _ in 0..COUNT {
-            let a = rng.gen_range(I8192::ONE..I8192::from(2).pow(NUM_WORDS as u32));
-            let b = rng.gen_range(I8192::ONE..I8192::from(2).pow(NUM_WORDS as u32));
-            let start = Instant::now();
-            sum_products += a * b;
-            total_duration += start.elapsed();
+        let np = mod_inverse(R - P, R);
+        let r2 = R % P * R % P;
+        let mut prod = R;
+        for num in nums {
+            let mnum = redc(num * r2, P, np);
+            prod = redc(prod * mnum, P, np);
+            // prod *= num;
+            // prod %= P;
         }
+        prod = redc(prod, P, np);
 
-        println!("Time elapsed: {:?}", total_duration);
-    }
-
-    #[test]
-    fn test_crypto_bigint() {
-        let mut rng = StdRng::seed_from_u64(0);
-
-        let mut total_duration = Duration::ZERO;
-
-        let mut sum_products: Wrapping<Uint<NUM_WORDS>> = Wrapping(Uint::ONE);
-        for _ in 0..COUNT {
-            let a = Wrapping(U8192::random(&mut rng));
-            let b = Wrapping(U8192::random(&mut rng));
-            let start = Instant::now();
-            sum_products += a * b;
-            total_duration += start.elapsed();
-        }
-
-        println!("Time elapsed: {:?}", total_duration);
-    }
-
-    #[test]
-    fn test_rug() {
-        let mut rng = RandState::new();
-
-        let mut total_duration = Duration::ZERO;
-
-        let mut sum_products = Integer::new();
-        sum_products.assign(Integer::ONE);
-        for _ in 0..COUNT {
-            let a = Integer::from(Integer::random_bits(64 * NUM_WORDS as u32, &mut rng));
-            let b = Integer::from(Integer::random_bits(64 * NUM_WORDS as u32, &mut rng));
-            let start = Instant::now();
-            sum_products += a * b;
-            total_duration += start.elapsed();
-        }
-
-        println!("Time elapsed: {:?}", total_duration);
+        println!("Product: {} ({:?})", prod, start.elapsed());
+        assert_eq!(prod, 450032353);
     }
 }
